d3983c2e4e228a7bf1875ce182e84dc67f43d140
dinushchathurya <dinushchathurya21@gmail.com>
2022-06-11 11:12:49 +0530

    config: Peering databse connection in index.js
    Signed-off-by: dinushchathurya <dinushchathurya21@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const colors = require('colors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const connectDB = require("./config/db");
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+// Connect to database
+connectDB();
+
+app.use(cors());
+
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    graphiql: process.env.NODE_ENV === 'development'
+    graphiql: true,
 }));
 
 app.listen(port, () => {
