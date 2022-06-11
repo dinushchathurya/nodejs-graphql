43e97d75dd6f394f0338df2f2e243e7ac27ecf9d
dinushchathurya <dinushchathurya21@gmail.com>
2022-06-11 08:53:47 +0530

    feat: Import graphql to application

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
